43841745186fc65d59e156ce7b650e1d735433ad
Godzilla-Q <teresa@tbi.univie.ac.at>
2016-07-18 20:27:03 +0200

    Calculate and plot bppm for sample Seq

--- pythons/plot_pf.py
+++ pythons/plot_pf.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 
-"""plot the partition function"""
+"""calculate and plot the base pair probability matrix"""
 
 # from __future__ import print_function
-import sys
+
 import RNA
 import matplotlib
 import Bio
-from Bio import SeqIO
+import matplotlib.pyplot as pyplot
 from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
 
-godzilla = Seq('GAGACCCGTAAAAGGGTCTCGAAA', IUPAC.unambiguous_dna)
+# Define sample sequence
+godzilla = Seq('GAGACCCGTAAAAGGGTCTCGAAAGTGTGTAAAAAACACAC')
 godzilla.id = 'Godzilla Queen of Monsters'
-monster = str(godzilla)
-foldgod = RNA.fold_compound(monster)
-
-# calculate mfe structure and mfe
-struct, mfe = foldgod.mfe()
+foldgod = RNA.fold_compound(str(godzilla))
 
 # calculate partition function for both temperatures
 pfstruct, pf = foldgod.pf()
@@ -27,30 +23,9 @@ bppm = foldgod.bpp()
 
 # print sequence name, mfes and partition functions for both temperatures
 print godzilla.id
-print mfe
-print pf
-print bppm
 
 # plot base pair probability matrix
-
-import matplotlib.pyplot as pyplot
-import numpy
-
-def samplemat(dims):
-    """Make a matrix with all zeros and increasing elements on the diagonal"""
-    aa = numpy.zeros(dims)
-    for i in range(min(dims)):
-        aa[i, i] = i
-    return aa
-
-# Display 2 matrices of different sizes
-dimlist = [(15, 20)]
-for d in dimlist:
-    pyplot.matshow(samplemat(d))
-
-# Display a random matrix with a specified figure number and a grayscale
-# colormap
-pyplot.matshow(numpy.random.rand(64, 64), fignum=100, cmap=pyplot.cm.gray)
+pyplot.matshow(bppm, fignum=godzilla.id)
 
 pyplot.show()
 

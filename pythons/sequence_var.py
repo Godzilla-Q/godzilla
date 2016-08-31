790ae74c4e10eedb9f08d6fbcab7a06c87152743
Godzilla-Q <teresa@tbi.univie.ac.at>
2016-08-31 23:19:39 +0200

    Add calculation of GC content

--- pythons/sequence_var.py
+++ pythons/sequence_var.py
@@ -7,9 +7,66 @@ import RNA
 import re
 from Bio import SeqIO
 from Bio.Seq import Seq
+from Bio.SeqUtils import GC
 from ss_dotplot import versions_used
 from itertools import product
 from operator import itemgetter
+
+
+############### new version of script, work in progress!
+
+def temperature_reactivity( sequence, structure, temperature1, temperature2 ):
+    """Evaluate temperature-dependent difference in energy, entropy, enthalpy."""
+    temperature_model1 = RNA.md()
+    temperature_model2 = RNA.md()
+    temperature_model1.temperature = temperature1
+    temperature_model2.temperature = temperature2
+    fc1 = RNA.fold_compound(sequence, temperature_model1)
+    fc2 = RNA.fold_compound(sequence, temperature_model2)
+    energy_of_struct1 = fc1.eval_structure(structure)
+    energy_of_struct2 = fc2.eval_structure(structure)
+    delta_energy = energy_of_struct2 - energy_of_struct1
+    delta_entropy = (-delta_energy/(temperature2 - temperature1))
+    delta_enthalpy = (energy_of_struct1 + temperature1 * delta_entropy)
+#    delta_enthalpy2 = (energy_of_struct2 + temperature2 * delta_entropy)    
+    return (delta_energy, delta_entropy, delta_enthalpy)
+
+def combinations( stem_length, loop_length ):
+    """Yield all sequence combinations for a stem loop with fixed loop"""
+    basepair_set = ['au', 'ua', 'gc', 'cg', 'gu', 'ug']
+    center = 'a' * loop_length    
+    for basepairs in product(basepair_set, repeat = stem_length):
+        left, right = zip(*basepairs)
+        sequence = (''.join(left), center, ''.join(right[::-1])) 
+        yield ''.join(sequence)
+
+def sort_results( list_of_tuples, number ):
+    """Sort list of tuples with respect to the attribute number 'number'"""
+    return sorted(list_of_tuples, key=itemgetter(number))
+
+def sequence_properties( sequence ):
+    """Calculate GC content"""
+    return GC(sequence)
+
+def main():
+    """calculate parameters for all sequences variations, print them sorted by their temperature reactivity"""
+    structure = '(((......)))'
+    results = []
+    for sequence in combinations(3, 6):
+        energy, entropy, enthalpy = temperature_reactivity(sequence, structure, 30, 37)
+        gc_content = sequence_properties(sequence)
+        results.append((sequence, energy, entropy, enthalpy, gc_content))
+    for number in range(1, 4):
+        for line in sort_results(results, number):
+            print line[0], line[1], line[2], line[3], line[4]
+        print '\n'
+
+    
+if __name__ == '__main__':
+    sys.exit(main())
+ 
+
+    
 
 ############### old version of the script, switching basepairs of a given stem
 
@@ -58,52 +115,4 @@ def oldmain():
         new = switch_bp(original,a,b)
         print new.seq
     print versions_used()
-
-############### new version of script, work in progress!
-
-def calculate_values( sequence, structure, temperature1, temperature2 ):
-    """Evaluate energy, entropy, enthalpy for each temperature"""
-    temperature_model1 = RNA.md()
-    temperature_model2 = RNA.md()
-    temperature_model1.temperature = temperature1
-    temperature_model2.temperature = temperature2
-    fc1 = RNA.fold_compound(sequence, temperature_model1)
-    fc2 = RNA.fold_compound(sequence, temperature_model2)
-    energy_of_struct1 = fc1.eval_structure(structure)
-    energy_of_struct2 = fc2.eval_structure(structure)
-    delta_energy = energy_of_struct2 - energy_of_struct1
-    delta_entropy = (-delta_energy/(temperature2 - temperature1))
-    delta_enthalpy = (energy_of_struct1 + temperature1 * delta_entropy)
-#    delta_enthalpy2 = (energy_of_struct2 + temperature2 * delta_entropy)    
-    return (delta_energy, delta_entropy, delta_enthalpy)
-
-def combinations( stem_length, loop_length ):
-    """Yield all sequence combinations for a stem loop with fixed loop"""
-    basepair_set = ['au', 'ua', 'gc', 'cg', 'gu', 'ug']
-    center = 'a' * loop_length    
-    for basepairs in product(basepair_set, repeat = stem_length):
-        left, right = zip(*basepairs)
-        sequence = (''.join(left), center, ''.join(right[::-1])) 
-        yield ''.join(sequence)
-
-def sort_results( list_of_tuples, number ):
-    return sorted(list_of_tuples, key=itemgetter(number))
-        
-def main():
-    structure = '((((......))))'
-    results = []
-    for sequence in combinations(3, 6):
-        energy, entropy, enthalpy = calculate_values(sequence, structure, 30, 37)
-        results.append((sequence, energy, entropy, enthalpy))
-    for number in range(1, 4):
-        for line in sort_results(results, number):
-            print line[0], line[1], line[2], line[3]
-        print '\n'
-
-    
-if __name__ == '__main__':
-    sys.exit(main())
- 
-
-    
 

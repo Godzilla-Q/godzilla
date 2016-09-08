33e18bf611729a41ce6dccb2f9852f46bb86f5b5
Godzilla-Q <teresa@tbi.univie.ac.at>
2016-09-08 20:38:15 +0200

    Update testing, add parser

--- pythons/sequence_var.py
+++ pythons/sequence_var.py
@@ -5,6 +5,7 @@
 import sys
 import RNA
 import re
+import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
@@ -31,8 +32,8 @@ def temperature_reactivity( sequence, structure, temperature1, temperature2 ):
 #    delta_enthalpy2 = (energy_of_struct2 + temperature2 * delta_entropy)    
     return (delta_energy, delta_entropy, delta_enthalpy)
 
-def combinations( stem_length, loop_length ):
-    """Yield all sequence combinations for a stem loop with fixed loop"""
+def variations( stem_length, loop_length ):
+    """Yield all sequence variations for a stem loop with fixed loop"""
     basepair_set = ['au', 'ua', 'gc', 'cg', 'gu', 'ug']
     center = 'a' * loop_length    
     for basepairs in product(basepair_set, repeat = stem_length):
@@ -48,12 +49,54 @@ def sequence_properties( sequence ):
     """Calculate GC content"""
     return GC(sequence)
 
+def argument_parser( arguments ):
+    """If arguments from command line are present, override default parameters. Ensure that sequence matches structure length. Manage help messages"""
+    
+    parser = argparse.ArgumentParser(
+        description='Generate variations of the bases of the stem; calculate the difference in thermodynamic parameters for two different temperatures.',
+        epilog='All hail Godzilla, Queen of monsters.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '-T', '--temperatures',
+        nargs=2,
+        default=[30, 37],
+        type=int,
+        help='two temperatures in degree Celsius',
+        metavar=('TEMPERATURE1', 'TEMPERATURE2')
+    )
+    parser.add_argument(
+        '-s', '--stem',
+         default=3,
+        type=int,
+        choices=xrange(2, 6),
+        help='length of stem'
+    )
+    parser.add_argument(
+        '-l', '--loop',
+         default=6,
+        type=int,
+        choices=xrange(3, 10),
+        help='length of loop'
+    )
+
+    return parser.parse_args(arguments)
+
+def create_structure( stem, loop ):
+    """Create structure string corresponding to the sequences."""
+    return ''.join(['('*stem, '.'*loop, ')'*stem])
+
 def main():
-    """calculate parameters for all sequences variations, print them sorted by their temperature reactivity"""
-    structure = '(((......)))'
+    """Generate variations of the bases of the stem; calculate the difference in
+thermodynamic parameters for two different temperatures."""
+    parser = argument_parser(sys.argv[1:])
+    temperature_1, temperature_2 = parser.temperatures
+    stem = parser.stem
+    loop = parser.loop
+    structure = create_structure(stem, loop)
     results = []
-    for sequence in combinations(3, 6):
-        energy, entropy, enthalpy = temperature_reactivity(sequence, structure, 30, 37)
+    for sequence in variations(stem, loop):
+        energy, entropy, enthalpy = temperature_reactivity(sequence, structure, temperature_1, temperature_2)
         gc_content = sequence_properties(sequence)
         results.append((sequence, energy, entropy, enthalpy, gc_content))
     for number in range(1, 4):
--- pythons/test_sequence_var.py
+++ pythons/test_sequence_var.py
@@ -4,6 +4,11 @@
 
 import unittest
 from sequence_var import temperature_reactivity
+from sequence_var import variations
+from sequence_var import sort_results
+from sequence_var import sequence_properties
+from sequence_var import argument_parser
+from sequence_var import create_structure
 
 import sys
 import RNA
@@ -15,16 +20,134 @@ from ss_dotplot import versions_used
 from itertools import product
 from operator import itemgetter
 
-#auaaaaaaauau (((......))) 30 37
-#0.430000066757 -0.0614285809653 0.927142551967
+GODZILLA = 'Queen of monsters'
 
 class TestReactivityValues(unittest.TestCase):
 
     def test_temperature_reactivity(self):
+        "Compare to previously calculated results"
         self.assertEqual(temperature_reactivity('auaaaaaaauau', '(((......)))', 30, 37), (0.43000006675720215, -0.061428580965314596, 0.9271425519670757))
         self.assertEqual(temperature_reactivity('aaaaaaaaaauuuu', '((((......))))', 30, 37), (0.5, -0.07142857142857142, 0.25714295251028885))
         self.assertEqual(temperature_reactivity('gggaaaaaauuu', '(((......)))', 30, 37), (0.6999998092651367, -0.0999999727521624, 1.4000009128025597))
         self.assertEqual(temperature_reactivity('gcccaaaaaagggc', '((((......))))', 30, 37), (1.0500001907348633, -0.15000002724783762, -11.650000912802561))
 
+class TestVariations(unittest.TestCase):
+    example_variations = ['aaaaauu',
+                          'auaaaau',
+                          'agaaacu',
+                          'acaaagu',
+                          'agaaauu',
+                          'auaaagu',
+                          'uaaaaua',
+                          'uuaaaaa',
+                          'ugaaaca',
+                          'ucaaaga',
+                          'ugaaaua',
+                          'uuaaaga',
+                          'gaaaauc',
+                          'guaaaac',
+                          'ggaaacc',
+                          'gcaaagc',
+                          'ggaaauc',
+                          'guaaagc',
+                          'caaaaug',
+                          'cuaaaag',
+                          'cgaaacg',
+                          'ccaaagg',
+                          'cgaaaug',
+                          'cuaaagg',
+                          'gaaaauu',
+                          'guaaaau',
+                          'ggaaacu',
+                          'gcaaagu',
+                          'ggaaauu',
+                          'guaaagu',
+                          'uaaaaug',
+                          'uuaaaag',
+                          'ugaaacg',
+                          'ucaaagg',
+                          'ugaaaug',
+                          'uuaaagg']
+
+    def test_variations(self):
+        "Check number of variations"
+        self.assertEqual(len(list(variations(3,1))), 216)
+        self.assertEqual(len(list(variations(4,20))), 1296)
+        self.assertEqual(list(variations(2,3)), self.example_variations)
+
+class TestSortResults(unittest.TestCase):
+    list_of_tuples_sortby_0 = [(0, 4, 3, 2, 1),
+                               (1, 0, 4, 3, 2),
+                               (2, 1, 0, 4, 3),
+                               (3, 2, 1, 0, 4),
+                               (4, 3, 2, 1, 0)]
+
+    list_of_tuples_sortby_1 = [(1, 0, 4, 3, 2),
+                               (2, 1, 0, 4, 3),
+                               (3, 2, 1, 0, 4),
+                               (4, 3, 2, 1, 0),
+                               (0, 4, 3, 2, 1)]
+
+    list_of_tuples_sortby_2 = [(2, 1, 0, 4, 3),
+                               (3, 2, 1, 0, 4),
+                               (4, 3, 2, 1, 0),
+                               (0, 4, 3, 2, 1),
+                               (1, 0, 4, 3, 2)]
+
+    list_of_tuples_sortby_3 = [(3, 2, 1, 0, 4),
+                               (4, 3, 2, 1, 0),
+                               (0, 4, 3, 2, 1),
+                               (1, 0, 4, 3, 2),
+                               (2, 1, 0, 4, 3)]
+
+    list_of_tuples_sortby_4 = [(4, 3, 2, 1, 0),
+                               (0, 4, 3, 2, 1),
+                               (1, 0, 4, 3, 2),
+                               (2, 1, 0, 4, 3),
+                               (3, 2, 1, 0, 4)]     
+    
+    def test_sort_results(self):
+        """Check sorting"""
+        self.assertEqual(sort_results(self.list_of_tuples_sortby_0, 0), self.list_of_tuples_sortby_0)
+        self.assertEqual(sort_results(self.list_of_tuples_sortby_0, 1), self.list_of_tuples_sortby_1)
+        self.assertEqual(sort_results(self.list_of_tuples_sortby_0, 2), self.list_of_tuples_sortby_2)
+        self.assertEqual(sort_results(self.list_of_tuples_sortby_0, 3), self.list_of_tuples_sortby_3)
+        self.assertEqual(sort_results(self.list_of_tuples_sortby_0, 4), self.list_of_tuples_sortby_4)
+
+class TestSequenceProperties(unittest.TestCase):
+    def test_sequence_properties(self):
+        """Check sequence properties results"""
+        self.assertTrue(sequence_properties('ccggg') == 100)
+        self.assertTrue(sequence_properties('ccgga') == 80)
+        self.assertTrue(sequence_properties('ccgau') == 60)
+        self.assertTrue(sequence_properties('ccaua') == 40)
+        self.assertTrue(sequence_properties('cuaua') == 20)
+        self.assertTrue(sequence_properties('auaua') == 0)
+        
+
+class TestParser(unittest.TestCase):
+    def test_parser(self):
+        """Test parsing of user input"""
+        long_input = argument_parser(['--stem', '5', '--temperatures', '20', '40', '--loop', '8'])
+        self.assertEqual(long_input.stem, 5)
+        self.assertEqual(long_input.loop, 8)
+        self.assertEqual(long_input.temperatures, [20, 40])
+        short_input = argument_parser(['-l', '3', '-s', '2', '-T', '12', '80'])
+        self.assertEqual(short_input.stem, 2)
+        self.assertEqual(short_input.loop, 3)
+        self.assertEqual(short_input.temperatures, [12, 80])
+        no_input = argument_parser('')
+        self.assertEqual(no_input.stem, 3)
+        self.assertEqual(no_input.loop, 6)
+        self.assertEqual(no_input.temperatures, [30, 37])
+
+class TestCreateStructure(unittest.TestCase):
+    def test_create_structure(self):
+        """Test structure creation"""
+        self.assertEqual(create_structure(3,6), '(((......)))')
+        self.assertEqual(create_structure(8,5), '((((((((.....))))))))')
+
+
+        
 if __name__ == '__main__':
     unittest.main()

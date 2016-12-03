c6730f7cef522000ebf84efbdd993c30acbf1f48
Godzilla-Q <teresa@tbi.univie.ac.at>
2016-12-03 14:11:07 +0100

    Add calulation of peripheral/central paired/unpaired bases

--- pythons/bpstats.py
+++ pythons/bpstats.py
@@ -10,7 +10,7 @@ from subprocess import Popen, PIPE
 from seq_properties import argument_parser
 from seq_properties import temperature_reactivity
 
-def bp_stats( sequence, structure ): # work in progress!
+def bp_stats_old( sequence, structure ):
     """Calculate GC content and other stats"""
     # read sequence and structure and turn them into lists I can use
     ptable = list(RNA.ptable(structure))
@@ -38,17 +38,87 @@ def bp_stats( sequence, structure ): # work in progress!
     results.append(GC(sequence))
     return results
 
+def bp_stats( sequence, structure ): # work in progress!
+    """Calculate GC content and other stats"""
+    # read sequence and structure and turn them into lists I can use
+    ptable = list(RNA.ptable(structure))
+    length = ptable.pop(0)
+    seq_l = list(sequence)
+    # make anoher list containing the pairing partners
+    pair_partners = []
+    for i in ptable:
+        if i != 0:
+            pair_partners.append(sequence[i - 1])
+        else:
+            pair_partners.append('-')
+    # make a list of tuples containing the basepairs
+    pairlist = zip(seq_l, pair_partners)
+    
+    # make a list indicating whether the bp/unpaired base sits in the middle or on the edge
+    mrlist = []
+    
+    structure_l = list(structure)
+    if structure_l[0] == structure_l[1]:
+        mrlist.append('m')
+    else:
+        mrlist.append('r')
+    for i, j, k in zip(structure_l, structure_l[1:], structure_l[2:]):
+        if i == j == k:
+            mrlist.append('m')
+        else:
+            mrlist.append('r')
+    if structure_l[-1] == structure_l[-2]:
+        mrlist.append('m')
+    else:
+        mrlist.append('r')
+    
+    mrlist.append('m')
+    # split pairlist in two lists, one containing the middle pairs, one the edge pairs
+    list_m = []
+    list_r = []
+    for i, j in zip(pairlist, mrlist):
+        if j == 'm':
+            list_m.append(i)
+        if j == 'r':
+            list_r.append(i)
+    
+    # count all possible basepairs and all unpaired bases in the middle list
+    AU_m = list_m.count(('A', 'U')) + list_m.count(('U', 'A'))
+    GU_m = list_m.count(('G', 'U')) + list_m.count(('U', 'G'))
+    GC_m = list_m.count(('G', 'C')) + list_m.count(('C', 'G'))    
+    A_m = list_m.count(('A', '-'))
+    C_m = list_m.count(('C', '-'))
+    G_m = list_m.count(('G', '-'))
+    U_m = list_m.count(('U', '-'))
+    results_m = [AU_m, GU_m, GC_m, A_m, C_m, G_m, U_m]
+    # count all possible basepairs and all unpaired bases in the edge list    
+    AU_r = list_r.count(('A', 'U')) + list_r.count(('U', 'A'))
+    GU_r = list_r.count(('G', 'U')) + list_r.count(('U', 'G'))
+    GC_r = list_r.count(('G', 'C')) + list_r.count(('C', 'G'))    
+    A_r = list_r.count(('A', '-'))
+    C_r = list_r.count(('C', '-'))
+    G_r = list_r.count(('G', '-'))
+    U_r = list_r.count(('U', '-'))
+    results_r = [AU_r, GU_r, GC_r, A_r, C_r, G_r, U_r]
+    
+    results = results_m + results_r
+    results = [float(i) / length for i in results]
+    results.append(GC(sequence))
+    return results
+
 def main(): 
     """Calculate the difference in thermodynamic parameters for two different temperatures and calculate descriptors for the sequence"""
-    print 'sequence, structure, energy_of_struct1, energy_of_struct2, gc_content, AU, GU, GC, Aunp, Cunp, Gunp, Uunp, delta_energy'
+    print 'sequence, structure, energy_of_struct1, energy_of_struct2, AU_m, GU_m, GC_m, A_m, C_m, G_m, U_m, AU_r, GU_r, GC_r, A_r, C_r, G_r, U_r, GCcontent, delta_energy'
     parser = argument_parser(sys.argv[1:])
     temperature_1, temperature_2 = parser.temperatures
     for line in sys.stdin:
         sequence, structure = line.split()
         delta_energy, energy_of_struct1, energy_of_struct2 = temperature_reactivity(sequence, structure, temperature_1, temperature_2)
-        [AU, GU, GC, Aunp, Cunp, Gunp, Uunp, GCcontent] = bp_stats(sequence, structure)
-        print sequence, structure, energy_of_struct1, energy_of_struct2, GCcontent, AU, GU, GC, Aunp, Cunp, Gunp, Uunp, delta_energy
-
+        [AU_m, GU_m, GC_m, A_m, C_m, G_m, U_m, AU_r, GU_r, GC_r, A_r, C_r, G_r, U_r, GCcontent] = bp_stats(sequence, structure)
+        results = [sequence, structure, energy_of_struct1, energy_of_struct2, AU_m, GU_m, GC_m, A_m, C_m, G_m, U_m, AU_r, GU_r, GC_r, A_r, C_r, G_r, U_r, GCcontent, delta_energy]
+        print ', '.join(map(str, results))
+            
     
 if __name__ == '__main__':
     sys.exit(main())
+
--- pythons/test_bpstats.py
+++ pythons/test_bpstats.py
@@ -4,22 +4,41 @@
 
 import unittest
 
+from bpstats import bp_stats_old
 from bpstats import bp_stats
 
 GODZILLA = 'Queen of monsters'
 
 class TestBPStats(unittest.TestCase):
-    def test_bp_stats(self):
+    def test_bp_stats_old(self):
         "Compare to previously calculated results"
 
-        self.assertTrue(bp_stats('AAAAA', '.....') == [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0])
-        self.assertTrue(bp_stats('CCCCC', '.....') == [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 100.0])
-        self.assertTrue(bp_stats('GGGGG', '.....') == [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 100.0])
-        self.assertTrue(bp_stats('UUUUU', '.....') == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
-        self.assertTrue(bp_stats('AUAGGGGGGUAU', '(((......)))') == [0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 50.0])
-        self.assertTrue(bp_stats('GGGAAAAAAUUU', '(((......)))') == [0.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.0, 25.0])
-        self.assertTrue(bp_stats('GGGAAAAAACCC', '(((......)))') == [0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 50.0])
-        self.assertFalse(bp_stats('AAAAA', '.....') == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.assertEqual(bp_stats_old('AAAAA', '.....'), [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+        self.assertEqual(bp_stats_old('CCCCC', '.....'), [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 100.0])
+        self.assertEqual(bp_stats_old('GGGGG', '.....'), [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 100.0])
+        self.assertEqual(bp_stats_old('UUUUU', '.....'), [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
+        self.assertEqual(bp_stats_old('AUAGGGGGGUAU', '(((......)))'), [0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 50.0])
+        self.assertEqual(bp_stats_old('GGGAAAAAAUUU', '(((......)))'), [0.0, 0.5, 0.0, 0.5, 0.0, 0.0, 0.0, 25.0])
+        self.assertEqual(bp_stats_old('GGGAAAAAACCC', '(((......)))'), [0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 50.0])
+        self.assertFalse(bp_stats_old('AAAAA', '.....') == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    def test_bp_stats(self):
+        structure1 = '....((((.....))))........((((...))))(((((((.(((((((.......)))))))...)))))))....((((.......))))......'
+        sequence1 = 'AAGUCGAAACCGGUUUGUACUGCCGGGUCAAAGACCUGUCGUUAAAGUUUUAUCGCGGAGGGUUUUAAAGCGGCAAACGCGAUUUAGUCGAUCGCCUCAA'
+
+        expected_results1 = [0.08, 0.14, 0.10, 0.09, 0.11, 0.06, 0.05, 0.12, 0.0, 0.08, 0.07, 0.01, 0.05, 0.04, 48.0]
+        self.assertEqual(bp_stats(sequence1, structure1), expected_results1)
+
+        structure2 = '.(((......))).'
+        sequence2 = 'AGUGCUCCCACACC'
+
+        expected_results2 = [0.14285714285714285, 0.0, 0.0, 0.0, 0.21428571428571427, 0.0, 0.07142857142857142, 0.0, 0.0, 0.2857142857142857, 0.14285714285714285, 0.14285714285714285, 0.0, 0.0, 64.28571428571429]
+
+        self.assertEqual(bp_stats(sequence2, structure2), expected_results2)
+        
+
+    
+
         
 if __name__ == '__main__':
     unittest.main()
